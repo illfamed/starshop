0e7ebc73a40be001802639aabff94a39745d60c1
illfamed <generalarmyhf@gmail.com>
2024-10-21 19:31:06 +0200

    ep9 done

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,8 +11,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class StarshipApiController extends AbstractController
 {
   #[Route('/api/starships')]
-  public function getCollection(): Response
+  public function getCollection(LoggerInterface $logger): Response
   {
+    $logger->info('Starship collection retrieved');
     $starships = [
       new Starship(
         1,
