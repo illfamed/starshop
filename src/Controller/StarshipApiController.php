aa32cd09e121c4fae5d58545c07bc767f484fcba
illfamed <generalarmyhf@gmail.com>
2024-10-21 20:35:57 +0200

    ep11 done

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -9,13 +9,26 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-  #[Route('/api/starships')]
+  #[Route('', methods: ['GET'])]
   public function getCollection(StarshipRepository $repository): Response
   {
     $starships = $repository->findAll();
 
     return $this->json($starships);
+  }
+  
+  #[Route('/{id<\d+>}', methods: ['GET'])]
+  public function get(int $id, StarshipRepository $repository): Response
+  {
+    $starship = $repository->find($id);
+
+    if (!$starship) {
+      throw $this->createNotFoundException('Starship not found');
+    }
+
+    return $this->json($starship);
   }
 }
